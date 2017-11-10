843d229285f160b344c24d5d3c95d7ca488fe47c
xxxgitone <xxxgit@sina.com>
2017-11-10 23:41:26 +0800

    快速排序

---
+++ sort/sort-core/selectionSort.js
@@ -0,0 +1,25 @@
+/**
+ * 选择排序
+ * @param {Array} arr 
+ * @param {length} n 
+ */
+function selectionSort (arr, n ) {
+
+  for (var i = 0; i < n; i++) {
+
+    // 寻找[i, n)区间里最小值,先假设这个区间的第一个数就是最小的(初始化)
+    var minIndex = i
+    for (var j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+
+    swap(i, minIndex)
+  }
+
+}
+
+var array = [10,2,5,4,8,9,6,8,7,4]
+selectionSort(array, 10)
+console.log(array)
---
+++ sort/util.js
@@ -0,0 +1,5 @@
+function swap (index1, index2) {
+  var temp = array[index1]
+  array[index1] = array[index2]
+  array[index2] = temp
+}
