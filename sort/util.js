620def53fd062412dd096038c8d8a336080de0ca
xxxgitone <xxxgit@sina.com>
2017-11-11 23:28:28 +0800

    测试的工具函数

--- sort/util.js
+++ sort/util.js
@@ -3,3 +3,30 @@ function swap (array, index1, index2) {
   array[index1] = array[index2]
   array[index2] = temp
 }
+
+// 生成有n个元素的随机数组，每个元素的随机范围为[rangel,rangeR]
+function generateRandomArray (n, rangeL, rangeR) {
+  var array = []
+  for (var i = 0; i < n; i++) {
+    array[i] = Math.floor(Math.random() * (rangeR - rangeL + 1) + rangeL)
+  }
+  return array
+}
+
+// 测试是否排序完成
+function isSorted (array, n) {
+  for (var i = 0; i < n - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      return false
+    }
+  }
+  return true
+}
+
+// 测试性能
+function testSort (sortName, fun, arr, n) {
+  console.time(sortName)
+  fun(arr,n)
+  console.timeEnd(sortName)
+  console.log(isSorted(arr, n))
+}
