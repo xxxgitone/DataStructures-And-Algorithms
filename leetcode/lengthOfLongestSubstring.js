43f4b4e51b916e758915fb4d2af5137aefb79755
xxxgitone <xxxgit@sina.com>
2018-01-07 22:16:19 +0800

    length of longest substring

--- leetcode/lengthOfLongestSubstring.js
+++ leetcode/lengthOfLongestSubstring.js
@@ -8,32 +8,25 @@ Desc:
  * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  * 
  * 就是求一个字符串里面不重复字符串的最大长度
+ * 例如 pwwkew，从首位开始，p, pw, pww，重复，继续查找: w, wk, wke, wkew, 重复，故最长不重复字符串为 wke，长度为 3
 */
 /**
 * @param {string} s
 * @return {number}
-* 遍历数组，并已值为键，记录其索引和子字符串起始位置 start
-* 遇见重复的元素后（当前索引为 i，重复元素的索引为 index），比较 index - start, i - start, i - index
-* 取最长，并更新 start
+* 遍历数组，并以值为键，索引为值
 */
 var lengthOfLongestSubstring = function(s) {
- var temp = {}
- var tempLength = 0
- var max = 0
- var start = 0
+  var map = {}
+  var start = 0 // 子字符串的起始位置
 
- for (var i = 0; i < s.length; i += 1) {
-   var str = s[i]
-   if (temp[str] === undefined || temp[str] < start) {
-     tempLength += 1
-   } else {
-     var index = temp[str]
-     max = Math.max(max, index - start, i - index, i - start)
-     start = index + 1
-     tempLength = i - index
-    }
-    temp[str] = i
-  }
-  max = Math.max(max, tempLength)
-  return max
+  // reduce的回调函数有四个参数:第一个为累加器,第二个为当前值,第三个为当前索引(可选),第四个为遍历的数组(可选)
+  return s.split('').reduce((max, value, index) => {
+    // 如果出现重复值,则start+1
+    start = map[value] >= start ? map[value] + 1 : start
+    map[value] = index
+    
+    // 每次返回最大长度
+    return Math.max(max, index - start + 1)
+  }, 0)
 }
+console.log(lengthOfLongestSubstring('pwwkew'))
