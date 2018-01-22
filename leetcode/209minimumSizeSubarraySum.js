94dcbc17031aeaa7411d0ca5952394d2f482d8eb
xxxgitone <xxxgit@sina.com>
2018-01-22 22:22:06 +0800

    leetCode :smile:

---
+++ leetcode/209minimumSizeSubarraySum.js
@@ -0,0 +1,10 @@
+/**
+ * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint.
+
+给定一个整型数组和一个数字s,找到数组中最短的一个连续子数组,使得连续子数组的数字和sum>=s
+返回这个最短的连续子数组的长度值
+
+ */
