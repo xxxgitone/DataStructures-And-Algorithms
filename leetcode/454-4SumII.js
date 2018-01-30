c30ab0b401066c3b21d490d75d61452cfc9c9fc8
xxxgitone <xxxgit@sina.com>
2018-01-30 23:14:53 +0800

    leetCode :smile:

--- leetcode/454-4SumII.js
+++ leetcode/454-4SumII.js
@@ -1,7 +1,7 @@
 /**
  * Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
 
-To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 2^31 - 1.
+To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -2^28 to 2^28 - 1 and the result is guaranteed to be at most 2^31 - 1.
 
 Example:
 
@@ -18,10 +18,13 @@ Explanation:
 The two tuples are:
 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+
+给出四个整型数组ABCD,寻找有多少ijkl的组合,使得A[i] + B[j] + C[k] + D[l] === 0
  */
 
 var fourSumCount = function (A, B, C, D) {
   var record = {}
+  // 先将CD可能出现的和存起来,放入查找表
   for (var i = 0; i < C.length; i++) {
     for (var j = 0; j < D.length; j++) {
       if (record[C[i] + D[j]]) {
@@ -31,5 +34,17 @@ var fourSumCount = function (A, B, C, D) {
       }
     }
   }
+
+  var res = 0
+  for (var i = 0; i < A.length; i++) {
+    for (var j = 0; j < B.length; j++) {
+      if (record[0 - A[i] - B[j]]) {
+        res += record[0 - A[i] - B[j]]
+      }
+    }
+  }
+  return res
 }
+
+console.log(fourSumCount([ 1, 2],[-2,-1],[-1, 2],[ 0, 2]))
 
