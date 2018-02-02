6ce5a1d3753eb68ed2a7cca1fb7d5faca28bd8e6
xxxgitone <xxxgit@sina.com>
2018-02-02 23:31:07 +0800

    leetCode :smile:

---
+++ leetcode/219containsDuplicateII.js
@@ -0,0 +1,14 @@
+/**
+ * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k
+ * 
+ * 给出一个整型数组nums和一个整数k,是否存在索引i和j,使得nums[i] == nums[j]且i和j之间的差不超过k
+ */
+
+ /**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+
+}
