a8222704810639cd3c35f7b85ccdbe3f3379a6d2
xxxgitone <xxxgit@sina.com>
2018-02-03 16:04:15 +0800

    leetCode :smile:

---
+++ leetcode/220containsDuplicateIII.js
@@ -0,0 +1,18 @@
+/**
+ * Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+ * 
+ * 给出整型数组nums,是否存在索引i和j,使得nums[i]和nums[j]之间的差不超过给定的数组t,且i和j之间的差别不超过给定的整数k
+ * 1.暴力解法
+ * 
+ * 2. |v-x| <= t ==> v
+ */
+
+ /**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, k, t){
+    
+}
