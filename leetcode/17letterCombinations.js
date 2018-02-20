c2a88adf87ae966bea09d10fbe2a9cf7c18b5d32
xxxgitone <xxxgit@sina.com>
2018-02-20 23:53:41 +0800

    leetCode :smile:

---
+++ leetcode/17letterCombinations.js
@@ -0,0 +1,19 @@
+/**
+ * Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+给出一个数字字符串,返回这个数字字符串表示的所有字母组合
+
+ */
+
+ /**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  
+}
