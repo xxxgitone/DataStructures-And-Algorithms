c7db28558a3c1030766764ec702909992cfdc527
xxxgitone <xxxgit@sina.com>
2018-03-02 23:15:25 +0800

    leetCode :smile:

---
+++ leetcode/200numberOfIslands.js
@@ -0,0 +1,32 @@
+/**
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+
+11000
+11000
+00100
+00011
+
+Amswer: 3
+
+给出一个二维数组,只含有0和1两个字符,其中1代表陆地,0代表水域.
+横向和纵向的陆地链接成岛屿,被水域隔开.
+
+给出的地图中有多少岛屿
+ */
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    
+}
